01fc04248a6808c111f80c658d7b60c7b6a5d42b
Chenhao <jameschenhaoliu@gmail.com>
2021-08-19 17:25:51 +0100

    modified main.js to add a pop-up dialogue on click

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,8 @@
 const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello World!';
+myHeading.textContent = 'Hello World!'; //This is an one line comment
+/*This is a
+multi-line
+comment*/
+document.querySelector('html').onclick = function() {
+    alert("Ouch! Stop poking me!");
+}
